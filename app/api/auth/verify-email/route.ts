75f5decb17b759246110dba7d117b97a0ce62e61
Gabb <nasolgabrielr@gmail.com>
2026-01-18 18:26:31 +0800

    fix: Added handle errors in email verification process

--- app/api/auth/verify-email/route.ts
+++ app/api/auth/verify-email/route.ts
@@ -3,31 +3,43 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { token } = await req.json();
+  try {
+    const { token } = await req.json();
 
-  const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+    const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
 
-  const record = await prisma.emailVerificationToken.findUnique({
-    where: { tokenHash },
-  });
+    const record = await prisma.emailVerificationToken.findUnique({
+      where: { tokenHash },
+    });
 
-  if (!record || record.expires < new Date())
+    if (!record || record.expires < new Date())
+      return NextResponse.json(
+        {
+          ok: false,
+          error: "BAD_REQUEST",
+          message: "Token is invalid or expired",
+        },
+        { status: 400 },
+      );
+
+    await prisma.$transaction([
+      prisma.user.update({
+        where: { id: record.userId },
+        data: { emailVerified: new Date() },
+      }),
+      prisma.emailVerificationToken.delete({ where: { tokenHash } }),
+    ]);
+
+    return NextResponse.json({ ok: true, data: null }, { status: 200 });
+  } catch (error) {
+    console.error("Email verification failed", error);
     return NextResponse.json(
       {
         ok: false,
-        error: "BAD_REQUEST",
-        message: "Token is invalid or expired",
+        error: "INTERNAL_SERVER_ERROR",
+        message: "Unable to verify email at this time.",
       },
-      { status: 400 },
+      { status: 500 },
     );
-
-  prisma.$transaction([
-    prisma.user.update({
-      where: { id: record.userId },
-      data: { emailVerified: new Date() },
-    }),
-    prisma.emailVerificationToken.delete({ where: { tokenHash } }),
-  ]);
-
-  return NextResponse.json({ ok: true, data: null }, { status: 200 });
+  }
 }
